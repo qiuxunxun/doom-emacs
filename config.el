dbfaffd48e6d9a314b23bf52a4681e022c092f45
hsun <qiuxunxun@yahoo.com>
2022-06-22 00:48:44 +0800

    Update my name and email and Remove org-super-agenda

--- config.el
+++ config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+(setq user-full-name "hsun"
+      user-mail-address "qiuxunxun@yahoo.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
@@ -74,3 +74,5 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+(blink-cursor-mode t)
